09b8ee7901e70618bab76a57b7f2671ac4216957
Adriviciano <97399669+Adriviciano@users.noreply.github.com>
2022-06-03 14:55:13 +0200

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,60 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1 className="App-title">
+          Guiñote online
+        </h1>
+      </header>
+      <body className="App-body">
+        
+        <div id="playerDeck" className="App-body-deck">
+          <div className="App-body-deck-card">
+            <img className="App-body-deck-card-image" src="./cartas/dorso.png" alt="" />
+          </div>
+          <div className="App-body-deck-card">
+            <img className="App-body-deck-card-image" src="./cartas/dorso.png" alt="" />
+          </div>
+          <div className="App-body-deck-card">
+            <img className="App-body-deck-card-image" src="./cartas/dorso.png" alt="" />
+          </div>
+          <div className="App-body-deck-card">
+            <img className="App-body-deck-card-image" src="./cartas/dorso.png" alt="" />
+          </div>
+          <div className="App-body-deck-card">
+            <img className="App-body-deck-card-image" src="./cartas/dorso.png" alt="" />
+          </div>
+          <div className="App-body-deck-card">
+            <img className="App-body-deck-card-image" src="./cartas/dorso.png" alt="" />
+          </div>
+        </div>
+
+        <div id="iaDeck" className="App-body-deck">
+          <div className="App-body-deck-card">
+            <img className="App-body-deck-card-image" src="./cartas/dorso.png" alt="" />
+          </div>
+          <div className="App-body-deck-card">
+            <img className="App-body-deck-card-image" src="./cartas/dorso.png" alt="" />
+          </div>
+          <div className="App-body-deck-card">
+            <img className="App-body-deck-card-image" src="./cartas/dorso.png" alt="" />
+          </div>
+          <div className="App-body-deck-card">
+            <img className="App-body-deck-card-image" src="./cartas/dorso.png" alt="" />
+          </div>
+          <div className="App-body-deck-card">
+            <img className="App-body-deck-card-image" src="./cartas/dorso.png" alt="" />
+          </div>
+          <div className="App-body-deck-card">
+            <img className="App-body-deck-card-image" src="./cartas/dorso.png" alt="" />
+          </div>
+        </div>
+
+      </body>
+    </div>
+  );
+}
+
+export default App;
